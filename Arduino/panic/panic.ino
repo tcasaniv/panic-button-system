1657a517a6f4ad442f3acf2a6673ea11654a811b
Xenom Flauta Dulce <xenomflauta@gmail.com>
2021-12-28 00:42:29 -0500

    Botón envía datos por POST
    Al presionar el botón se logra que el pin D1 reciba voltaje, haciendo que ejecute la función "enviarData();". Esta envía una cadena JSON a la URL de la página web mediante el método POST con todos los datos de ubicación y de identificación del dispositivo.
    Luego libera recursos y espera 5 segundos para presionar el botón nuevamente.
    Sin embargo al liberar recursos aparece en consola un código de excepción, se vuelve a conectar al Wi-Fi y vuelve a estar todo normal.
    Funciona incluso con el error.

--- Arduino/panic/panic.ino
+++ Arduino/panic/panic.ino
@@ -9,27 +9,28 @@
 const char *ssid = "YOUR_SSID"; //Nombre de la red
 const char *password = "YOUR_PASSWORD"; //Contraseña
 
-String id = "NodeMCU-1405";
-String dispositivo = "ESP8266";
+String name = "ESP-82B2E6";
+String description = "NodeMCU (ESP-12E) - ESP8266";
 String latitude = "-16.4049287";
 String longitude = "-71.5287552";
 String type = "Emergency";
 String fabricacion = "19-12-2021";
+
+int estado = 0;
 
 void setup() {
   // Serial.begin(115200);
   Serial.begin(9600);
   delay(10);
 
-  //Configuración  del GPIO2
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-  
+  //Configuración  del GPIO5
+  pinMode(5, INPUT); //pin D1 (GPI05) como entrada
+    
   Serial.println();
   Serial.println();
   Serial.print("Conectandose a red : ");
   Serial.println(ssid);
-  
+  //ESP.eraseConfig();
   WiFi.begin(ssid, password); //Conexión a la red
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -39,15 +40,13 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void enviarData(){
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-    HTTPClient http;
-    WiFiClient client;
-    String datos_a_enviar = "id=" + id + "&dispositivo=" + dispositivo + "&latitude=" + latitude + "&longitude=" + longitude + "&type=" + type + "&fabricacion=" + fabricacion;
-
-    http.begin(client,"botondepanico.tk", 80, "/panic"); //HTTP
-    // http.begin(WiFiClient &client, const String& host, uint16_t port, const String& uri, bool https))
-    //http.begin("http://botondepanico.tk/panic");                         //Indicamos el destino
+    HTTPClient http; WiFiClient client;
+    String datos_a_enviar = "name=" + name + "&description=" + description + "&latitude=" + latitude + "&longitude=" + longitude + "&type=" + type + "&fabricacion=" + fabricacion;
+    
+    //http.begin(WiFiClient &client, const String& host, uint16_t port, const String& uri, bool https))
+    http.begin(client,"botondepanico.tk", 80, "/panic"); //Indicamos el destino
     http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Preparamos el header text/plain si solo vamos a enviar texto plano sin un paradigma llave:valor.
 
     int codigo_respuesta = http.POST(datos_a_enviar); //Enviamos el post pasándole, los datos que queremos enviar. (esta función nos devuelve un código que guardamos en un int)
@@ -62,8 +61,18 @@ void loop() {
     } else {
       Serial.print("Error enviando POST, código: ");
       Serial.println(codigo_respuesta);
-    } http.end(); //Libera recursos
+    } Serial.println("Liberando recursos: ");
+    http.end(); //Libera recursos
   } else {
     Serial.println("Error en la conexión WIFI");
   } delay(5000); //5 segundos para enviar de nuevo petición
+}
+
+void loop() {  
+  estado = digitalRead(5);
+  if(estado == 1){ //Si el botón es presionado
+    Serial.println("¡Botón presionado!");
+    enviarData();
+  } else { Serial.println("No presionado...");}
+  estado = 0;  
 }
